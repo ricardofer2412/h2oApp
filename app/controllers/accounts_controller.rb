9c8e9c6bee806dd6810fbf664a0336ffab35d84a
Richard Casanova <ricardofer2412@hotmail.com>
2018-03-15 15:00:08 -0400

    added fetched button

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -12,6 +12,78 @@ class AccountsController < ApplicationController
   def show
   end
 
+  def fetch_balance
+    require 'watir'
+    require 'nokogiri'
+    require 'open-uri'
+    require 'selenium-webdriver'
+
+    Account.all.each do |account|
+
+        # Credentials
+        username = "29629"
+        password = "1234"
+
+        #Get SimCards
+        #active
+
+        simcardNumber = account.simcardNumber
+
+        #open Browser
+        browser = Watir::Browser.new :chrome, headless: true
+        browser.goto  "https://www.h2odealer.com/mainCtrl.php?page=DbEquip"
+
+        #Login
+        browser.text_field(:name => "dc").set username
+        browser.text_field(:type => "password").set password
+        browser.input(:type => "image").click
+
+        #Get SimNumber
+        browser.option(:value => "GSM").click
+        browser.text_field(:id => "gsm_mdn_sim").set simcardNumber
+        browser.image(:src => "images/db/bt_submit.png").click
+
+        sleep (11)
+
+        if browser.element(:xpath, "//*[@id='rep_error_note']").text == "Cancelled"
+          phoneNumber = browser.element(:xpath, "//*[@id='rep_error_mdn']").text
+        else
+          phoneNumber = browser.element(:xpath, "//*[@id='rep_gsm_mdn']").text
+        end
+        sleep(1)
+        accountStatus = browser.element(:xpath, "//*[@id='rep_gsm_mdn_status']").text
+        expiredAccount =  browser.element(:xpath, "//*[@id='rep_error_note']").text
+        sleep(1)
+
+        browser.goto  "https://www.h2odealer.com/mainCtrl.php?page=DbBalance"
+
+        #get DbBalance
+        browser.option(:value => "GSM").click
+        browser.text_field(:id => "txtMDN").set phoneNumber
+        browser.image(:src => "images/db/bt_submit.png").click
+
+
+        sleep(2)
+        #collect Data
+        balance = browser.element(:xpath, "//*[@id='fcard_bal']").text
+        expiration = browser.element(:xpath, "//*[@id='exp']").text
+        puts "SimCard Number:" + simcardNumber
+        puts "Phone Number: " + phoneNumber
+        puts "Account Balance: " + balance
+        puts "Expiration Date: " + expiration
+        puts "Account Status: " + accountStatus
+        puts expiredAccount
+        account.update_attribute(:balance, balance)
+        account.update_attribute(:accountStatus, accountStatus)
+        account.update_attribute(:accountStatus, expiredAccount)
+        account.update_attribute(:phoneNumber, phoneNumber)
+        account.update_attribute(:expirationDate, expiration)
+
+
+      end
+  end
+
+
   # GET /accounts/new
   def new
     @account = Account.new
@@ -50,7 +122,6 @@ class AccountsController < ApplicationController
       end
     end
   end
-
   # DELETE /accounts/1
   # DELETE /accounts/1.json
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+post 'fetch_balance' => 'accounts#fetch_balance', as: :fetch_balance
+
 end
